de43c17a77d8fc8c8db81c10e0d5f839eb074e27
min-87 <33282348+min-87@users.noreply.github.com>
2018-11-17 17:08:18 +0200

    Рекурсивный вывод содержимого всех папок директории

--- directory_iterator.php
+++ directory_iterator.php
@@ -2,16 +2,17 @@
 
 $path = 'C:\\xampp\\htdocs';
 
-function walk($path,$depth=0){
-    $files=scandir($path);
+function walk($path, $depth = 0)
+{
+    $files = scandir($path);
     array_shift($files);
     array_shift($files);
-    foreach ($files as $file){
-        $tmp=$path."\\$file";
-        echo str_repeat('----',$depth).$file;
-        if (is_dir($tmp)){
+    foreach ($files as $file) {
+        $tmp = $path . "\\$file";
+        echo str_repeat('----', $depth) . $file;
+        if (is_dir($tmp)) {
             echo "[*]<br>";
-            walk($tmp,$depth+1);
+            walk($tmp, $depth + 1);
             continue;
         }
         echo '<br>';
