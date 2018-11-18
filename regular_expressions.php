73da732a81cab976fb02e7cc5d7d27f0d4ab3da3
min-87 <33282348+min-87@users.noreply.github.com>
2018-11-18 18:36:41 +0200

    Разделить текст на слова

--- regular_expressions.php
+++ regular_expressions.php
@@ -3,7 +3,7 @@
  * Разделить текст на слова
  */
 
-$str = 'Привет!Это хорошая вещь,или это плохая, а может.';
+$str='Привет!Это хорошая вещь,или это плохая, а может.';
 
-$arr = preg_split('/\\b\\W+/u', $str, -1, PREG_SPLIT_NO_EMPTY);
+$arr =preg_split('/\\b\\W+/u', $str, -1, PREG_SPLIT_NO_EMPTY);
 print_r($arr);
